d83e8f5f13508132fb0694dfd23064a4c9ddcf74
Troy Landers <tlanders@landersconsulting.biz>
2022-11-27 21:44:46 -0600

    added default route

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import RegisterPage from "./pages/RegisterPage";
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Redirect
 } from "react-router-dom";
+import {Navigate} from "react-router";
 
 function App() {
   return (
@@ -14,12 +16,12 @@ function App() {
       <div className="jumbotron">
         <div className="container">
           <div className="col-sm-8 col-sm-offset-2">
-
             <Router>
               <Routes>
                 <Route exact path={'/'} element={<HomePage/>}/>
                 <Route exact path={'/login'} element={<LoginPage/>}/>
                 <Route exact path={'/register'} element={<RegisterPage/>}/>
+                <Route path="*" element={<Navigate to="/" replace />} />
               </Routes>
             </Router>
           </div>
