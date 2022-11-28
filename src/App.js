f1cbd487d4c3c2700d9c55fd848316a542c3715d
Troy Landers <tlanders@landersconsulting.biz>
2022-11-27 21:13:47 -0600

    added basic routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
@@ -10,9 +15,13 @@ function App() {
         <div className="container">
           <div className="col-sm-8 col-sm-offset-2">
 
-            <HomePage/>
-            <LoginPage/>
-            <RegisterPage/>
+            <Router>
+              <Routes>
+                <Route exact path={'/'} element={<HomePage/>}/>
+                <Route exact path={'/login'} element={<LoginPage/>}/>
+                <Route exact path={'/register'} element={<RegisterPage/>}/>
+              </Routes>
+            </Router>
           </div>
         </div>
       </div>
